4b5c497900484e94805efb0633ca2776e54d15eb
Minh Nguyen <mnguyen4725@gmail.com>
2022-06-15 14:58:25 -0700

    Implemented design to embedded post from bot

--- src/main/java/Discord_bot/Commands/Music/TrackScheduler.java
+++ src/main/java/Discord_bot/Commands/Music/TrackScheduler.java
@@ -4,9 +4,14 @@ import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -15,26 +20,47 @@ public class TrackScheduler extends AudioEventAdapter{
     private final AudioPlayer player;
     private final BlockingQueue<AudioTrack> queue; //queue for tracks
     private final GuildMessageReceivedEvent event;
+    private final EmbedBuilder embed;
+    private MessageEmbed builtEmbed;
 
     public TrackScheduler(AudioPlayer player, GuildMessageReceivedEvent event){
         this.event = event;
         this.player = player;
         this.queue = new LinkedBlockingQueue<>();
+        this.embed = new EmbedBuilder();
+        embed.setColor(new Color(0x327da8));
+        embed.setThumbnail("https://i.pinimg.com/originals/a0/f7/5e/a0f75e40bc09ad8850447362d2e1756d.jpg");
+        builtEmbed = embed.build();
+
     }
 
     //queue track
     public void queue(AudioTrack track){
         if(!this.player.startTrack(track,true)){ //player start track if nothing is playing at the moment.
             this.queue.offer(track); //adds track to queue if a track is playing
+            printQueue();
+        }else{
+            //create embed
+            setUpdateEmbed(track);
+            printQueue();
         }
     }
 
     //goto next track
     public void nextTrack(){
         AudioTrack track = this.queue.poll();
-        AudioTrackInfo trackInfo = track.getInfo();
+        //If there is no next track, then stop player
+        if(track==null){
+            stopTrack();
+            return;
+        }
+
         this.player.startTrack(track,false);
-        event.getChannel().sendMessage("Now Playing " + trackInfo.title + " by " + trackInfo.author).queue();
+
+        //create embed
+        setUpdateEmbed(track);
+        printQueue();
+
     }
 
     //pause track
@@ -60,6 +86,7 @@ public class TrackScheduler extends AudioEventAdapter{
     //clear queue
     public void clearQueue(){
         queue.clear();
+        printQueue();
     }
 
     public void test(){
@@ -72,12 +99,26 @@ public class TrackScheduler extends AudioEventAdapter{
             System.out.println("Next Track: " + track.getInfo().title);
             nextTrack();
         }
-        if(queue.isEmpty()){
+        if(queue.isEmpty() && this.player.getPlayingTrack()==null){ //if track queue is empty, then disconnect the bot from Voice Channel
             System.out.println("Disconnecting...");
             AudioManager audioManager = event.getGuild().getAudioManager();
             audioManager.closeAudioConnection();
         }
+    }
 
+    public void printQueue(){
+        System.out.println("Queue: ");
+        for(AudioTrack track: queue){
+            System.out.println(track.getInfo().title);
+        }
+    }
+    public void setUpdateEmbed(AudioTrack track){
+        AudioTrackInfo trackInfo = track.getInfo();
+        embed.setTitle("**"+"Currently Playing:"+"**");
+        embed.setDescription("```"+trackInfo.title+"```\n"+
+                "_" +trackInfo.author+"_\n"+
+                "<" +trackInfo.uri+">");
+        event.getChannel().sendMessage(embed.build()).queue();
     }
 
 }
